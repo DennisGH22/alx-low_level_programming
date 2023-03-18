8f756cd1c86e4d160b9977cb671bcc57aa6727b3
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-18 23:23:35 +0200

    Puts funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle using the puts function
+ * main - Prints "Programming is like building a multilingual puzzle using the puts function.
  * 
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+    return (0);
 }
