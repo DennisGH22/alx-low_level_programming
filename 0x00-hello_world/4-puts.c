e01029ee17752d9f90509e7573fe1cd29aa4252e
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-18 23:02:02 +0200

    Puts funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual puzzle using the puts function
- *
+ * 
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+    return (0);
 }
