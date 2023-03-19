71cdc926ac747d6f4f4d705626a730a42ec4ecb3
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-19 02:33:31 +0200

    Write funtion

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19.
  *
- * Return: (1)
+ * Return: Always 1 (Standard Error).
 */
 
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 
 return (1);
 }
