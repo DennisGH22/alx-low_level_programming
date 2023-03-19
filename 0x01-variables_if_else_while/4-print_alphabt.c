cb66fdbd22da307d6801b3b79104717c3a532d60
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-20 00:05:41 +0200

    Print alphabets, skip q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints A to Z in lowercase except for p and e.
+ * main - Prints A to Z in lowercase except for q and e.
  *
  * Return: Always 0 (Success).
 */
@@ -12,7 +12,7 @@ char i;
 
 for (i = 'a'; i <= 'z'; i++)
 {
-if (i == 'p' || i == 'e')
+if (i == 'q' || i == 'e')
 {
 continue;
 }
