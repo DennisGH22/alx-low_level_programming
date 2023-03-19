a16f4ed76973ae8ca546371392b18181336d61a0
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-19 23:37:11 +0200

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints A to Z in lowercase.
+ *
+ * Return: Always 0 (Success).
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+
+return (0);
+}
