d10ba44bc7133d21ce9cfad422b02193efa3fe45
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-19 23:27:16 +0200

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints the last digit of a random number.
- * 
+ *
  * Return: Always 0 (Success).
 */
 int main(void)
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("The last digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 if (lastDigit == 0)
 {
-printf("The last digit of %d is %d and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 if (lastDigit < 6 && lastDigit != 0)
 {
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 
 return (0);
