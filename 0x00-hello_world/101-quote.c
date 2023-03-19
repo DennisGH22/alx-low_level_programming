a00acbb3d958c43be5b3d2a33ba5aa9ea61b1b8a
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-19 02:25:59 +0200

    Write funtion

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: (1)
+*/
+
+int main()
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+
+return (1);
+}
