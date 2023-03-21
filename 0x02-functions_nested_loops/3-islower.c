d139126468c430ae962b300f26831614e0df9912
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-21 23:54:58 +0200

    Check for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,16 @@
 
 /**
  * _islower - Checks for lowercase character.
+ * @c: Character checked.
  *
- * Return: 1 if c is lowercase and 0 otherwise. 
+ * Return: 1 if c is lowercase and 0 otherwise.
 */
 
 int _islower(int c)
 {
 if (islower(c))
 {
-    return (1);
+return (1);
 }
 
 return (0);
