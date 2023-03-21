a7dc05aa5c97357fb1d6d3704c7140c8226f53a9
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-22 01:02:59 +0200

    Computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,16 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @v: The value computed.
- * 
+ *
  * Return: The value computed.
 */
 
 int _abs(int v)
 {
+if (v < 0)
+{
+    v *= -1;
+}
+
 return (v);
 }
