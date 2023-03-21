e4afeafca45bd158851c16bed3079c548a2dcd97
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-22 01:36:33 +0200

    Prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,34 @@
 
 void print_to_98(int n)
 {
+if (n <= 98)
+{
 for (; n <= 98; n++)
 {
-    _putchar('0' + n);
-    _putchar(',');
-    _putchar(' ');
+_putchar('0' + n);
+_putchar(',');
+_putchar(' ');
+if (n == 98)
+{
+_putchar('0' + n);
+_putchar('\n');
+break;
+}
+}
+}
+else
+{
+for (; n >= 98; n++)
+{
+_putchar('0' + n);
+_putchar(',');
+_putchar(' ');
+if (n == 98)
+{
+_putchar('0' + n);
+_putchar('\n');
+break;
+}
+}
 }
 }
