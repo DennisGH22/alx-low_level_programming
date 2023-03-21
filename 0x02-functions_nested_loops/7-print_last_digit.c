e6df19469b4c36460366446f7b5358f518bd8687
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-22 00:48:04 +0200

    Prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,10 @@ int print_last_digit(int n)
 {
 int lastDigit;
 lastDigit = n % 10;
-if (!lastDigit > 0 || lastDigit != 0)
+if (lastDigit < 0)
 {
-    lastDigit *= -1;
+lastDigit *= -1;
 }
-
 _putchar('0' + lastDigit);
 
 return (lastDigit);
