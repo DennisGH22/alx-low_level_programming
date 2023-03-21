b33cc2f45d572337703207154f0171fab53b96de
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-22 01:42:31 +0200

    Prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - Prints all natural numbers up to 98.
  * @n: The argument number.
- * 
+ *
  * Return: Always 0 (Success).
 */
 
@@ -14,25 +14,31 @@ if (n <= 98)
 {
 for (; n <= 98; n++)
 {
-printf("%d, ", n);
 if (n == 98)
 {
 printf("%d", n);
 printf("\n");
 break;
+}
+else
+{
+printf("%d, ", n);
 }
 }
 }
 else
 {
-for (; n >= 98; n++)
+for (; n >= 98; n--)
 {
-printf("%d, ", n);
 if (n == 98)
 {
 printf("%d", n);
 printf("\n");
 break;
+}
+else
+{
+printf("%d, ", n);
 }
 }
 }
