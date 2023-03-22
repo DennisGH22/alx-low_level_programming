df56e0d4cd4f5243e30a0252fc627dabd5fce553
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-22 02:16:18 +0200

    Fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int i;
-long j = 1, k = 2, sum;
+long j = 1, k = 2, sum = k;
 
 for (i = 0; k + j < 4000000; i++)
 {
@@ -18,7 +18,6 @@ if (k % 2 == 0)
 {
 sum += k;
 }
-sum = k;
 j = k - j;
 }
 printf("%ld\n", sum);
