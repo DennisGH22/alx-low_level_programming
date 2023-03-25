67fad29efb2bd108d5f4d7f112b571c64c48ad74
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-25 16:00:38 +0200

    Draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@
 void print_line(int n)
 {
 	int i;
-	
+
 	if (n == 0 || n < 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
