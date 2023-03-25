f0bde5aa1f176a05cd5ab5914ccef91d2e9b1f93
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-25 19:29:14 +0200

    Draw a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,14 +19,14 @@ void print_triangle(int size)
 		{
             for (j = size - i; j > 1; j--)
             {
-                _putchar(' ');
+            	_putchar(' ');
             }
 
-			for (k = 0; k <= i; k++)
+			for (k = 0; k < i; k++)
 			{
 				_putchar('#');
 			}
-            
+			_putchar('#');
 			_putchar('\n');
 		}
 	}
