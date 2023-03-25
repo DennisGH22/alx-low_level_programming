5b16dfeb3af799306e162a08269dc21d2d28db98
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-25 15:04:31 +0200

    Print 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,12 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 13; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			_putchar('0' + j);
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
