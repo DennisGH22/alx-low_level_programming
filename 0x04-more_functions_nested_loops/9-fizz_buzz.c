86b8fe7bc0d9ee21bdb169af3bba7942914c5637
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-25 17:36:42 +0200

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main()
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			continue;
 		}
 
 		if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 			continue;
 		}
 
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 			continue;
 		}
 
