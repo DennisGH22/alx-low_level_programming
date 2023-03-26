f37e6a68e3371fc441ed379e35fcb73f7b1c9624
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-26 13:19:07 +0200

    Print the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,43 +8,46 @@
 
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int i;
+	unsigned long j = 0, k = 1, sum;
+	unsigned long l, m, n, o;
+	unsigned long p, q;
 
-	for (count = 0; count < 92; count++)
+	for (i = 0; i < 92; i++)
 	{
-		sum = fib1 + fib2;
+		sum = j + k;
 		printf("%lu, ", sum);
 
-		fib1 = fib2;
-		fib2 = sum;
+		j = k;
+		k = sum;
 	}
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+	l = j / 10000000000;
+	n = k / 10000000000;
+	m = j % 10000000000;
+	o = k % 10000000000;
 
-	for (count = 93; count < 99; count++)
+	for (i = 93; i < 99; i++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		p = l + n;
+		q = m + o;
+		if (m + o > 9999999999)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			p += 1;
+			q %= 10000000000;
 		}
 
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
+		printf("%lu%lu", p, q);
 
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		if (i != 98)
+		{
+			printf(", ");
+		}
+
+		l = n;
+		m = o;
+		n = p;
+		o = q;
 	}
 	printf("\n");
 
