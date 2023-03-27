0ab1cf190b87475a24fb64337327ec73ef8ca44d
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-27 22:24:52 +0200

    Return string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: String checked.
+ * 
+ * Return: String length.
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
