b935dc3a44d08b4490892526b722855aed0af722
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-27 23:43:02 +0200

    Print every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,12 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < i; j += 2)
-		{
-			_putchar(str[i]);
-		}
-		
+
+	}
+	
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
