0fbf20480e6ba411cb878b70715201f21d38dbfc
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-27 23:22:43 +0200

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: The string to print characters of.
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] = '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
