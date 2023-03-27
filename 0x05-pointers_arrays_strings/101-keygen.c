c26450a29e344af01bfe9def7843a13ad32c43c5
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-28 01:00:22 +0200

    Random password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,22 +11,12 @@
 
 int main()
 {
-	size_t i;
-
-	char password[9];
-
-	const char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
-
-	size_t charset_len = strlen(charset);
-
 	srand(time(NULL));
-
-	for (i = 0; i < 8; i++) {
-		password[i] = charset[rand() % charset_len];
+    int random_number = rand() % 2;
+    if (random_number == 0)
+	{
+        printf("Tada! Congrats\n");
 	}
-
-	password[8] = '\0';
-	printf("%s\n", password);
 	
 	return (0);
 }
