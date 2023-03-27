0faf42e71b165e724c4349580eb43402b9a44262
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-27 23:11:27 +0200

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@
 void rev_string(char *s)
 {
 	int i, length = strlen(s);
-    
-    for (i = 0; i < length / 2; i++) {
-        char temp = s[i];
-        s[i] = s[length-i-1];
-        s[length-i-1] = temp;
-    }
+
+	for (i = 0; i < length / 2; i++) {
+    	char temp = s[i];
+		s[i] = s[length-i-1];
+		s[length-i-1] = temp;
+	}
 }
