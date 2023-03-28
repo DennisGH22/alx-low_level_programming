8fe4c3b1a9b0da998751661b86c8b10431955d13
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-28 22:55:53 +0200

    Convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,20 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
-int d = 0;
-int n = 0;
-int digit = 0;
-
-while (s[i] != '\0') {
-    if (s[i] == '-') {
-        d++;
-    } else if (s[i] >= '0' && s[i] <= '9') {
-        digit = s[i] - '0';
-        if (d % 2 == 1) {
-            digit = -digit;
-        }
-        n = n * 10 + digit;
-    } else if (n != 0) { // Break if we've already parsed a number
-        break;
-    }
-    i++;
-}
-
-return n;
-
+	int i = 0, d = 0, n = 0, digit = 0;
+	
+	while (s[i] != '\0') {
+		if (s[i] == '-')
+			++d;
+		else if (s[i] >= '0' && s[i] <= '9') {
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+		}
+		else if (n != 0) // break if we've already parsed a number
+			break;
+		++i;
+	}
+	return (n);
 }
