c749b542cc0dbf6a8059940a263cc378fcaac745
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-03-31 05:48:24 +0200

    Initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string.
+ * @str: The capitalized string.
+ *
+ * Return: str
+*/
+
+char *cap_string(char *str)
+{
+	int i;
+	int len = strlen(str);
+	int capitalize_next = 1;
+
+	for (i = 0; i < len; i++)
+	{
+		if (isspace(str[i]))
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next)
+		{
+			str[i] = toupper(str[i]);
+			capitalize_next = 0;
+		}
+		else
+		{
+			str[i] = tolower(str[i]);
+		}
+		
+	}
+
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * leet - Encode a string into 1337.
+ * @s: The encoded string.
+ *
+ * Return: s
+*/
+
+int i;
+
+for (i = 0; i < strlen(str); i++)
+{
+	switch (str[i])
+	{
+		case 'A':
+		case 'a':
+			str[i] = '4';
+			break;
+		case 'E':
+		case 'e':
+			str[i] = '3';
+			break;
+		case 'G':
+		case 'g':
+			str[i] = '6';
+			break;
+		case 'I':
+		case 'i':
+			str[i] = '1';
+			break;
+		case 'O':
+		case 'o':
+			str[i] = '0';
+			break;
+		case 'S':
+		case 's':
+			str[i] = '5';
+			break;
+		case 'T':
+		case 't':
+			str[i] = '7';
+			break;
+		default:
+			break;
+	}
+}
