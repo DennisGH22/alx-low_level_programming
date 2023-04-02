7dbe8c1a93bc6283bd7d43fd4f2151a5b49c046c
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 23:23:31 +0200

    Print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints buffer of a char string
- * @b: buffer to print
- * @size: size of buffer
- * Return: nothing
+ * print_buffer - Prints buffer of a char string.
+ * @b: Buffer to print.
+ * @size: Size of buffer.
  */
 
 void print_buffer(char *b, int size)
@@ -12,36 +11,49 @@ void print_buffer(char *b, int size)
 	int i, j;
 
 	if (size <= 0)
+	{
 		printf("\n");
+	}
 	else
 	{
 		for (i = 0; i < size; i += 10)
 		{
 			printf("%08x: ", i);
-			j = 0;
-			while (j < 10)
+
+			for (j = 0; j < 10; j++)
 			{
 				if (j % 2 == 0 && j != 0)
+				{
 					printf(" ");
+				}
+
 				if ((j + i) > size - 1)
 				{
 					printf("  ");
 				}
 				else
+				{
 					printf("%.2x", b[i + j]);
-				j++;
+				}
 			}
+
 			printf(" ");
-			j = 0;
-			while (j < 10)
+
+			for (j = 0; j < 10; j++)
 			{
 				if ((j + i) > size - 1)
+				{
 					break;
+				}
+
 				if (b[j + i] <= 31 || b[j + 1] >= '~')
+				{
 					b[j + i] = '.';
+				}
+
 				putchar(b[j + i]);
-				j++;
 			}
+
 			printf("\n");
 		}
 	}
