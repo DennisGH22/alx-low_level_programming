0977de7668a104aeb35ad23a3568006131a20bb4
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 20:42:25 +0200

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,23 @@
 
 void print_number(int n)
 {
-	_putchar('0' + n);
+	if (n > 0)
+	{
+		if (n >= 10)
+		{
+			print_number(n / 10);
+		}
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n <= -10)
+		{
+			print_number(n / -10);
+		}
+		_putchar('0' - n % 10);
+	}
 
 	return (0);
 }
