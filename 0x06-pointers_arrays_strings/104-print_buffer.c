ccde6d9ffd31417349cada443d087a918a65c543
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 23:16:57 +0200

    Print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,60 +1,48 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer.
- * @b: The buffer to be printed.
- * @size: The size the buffer.
+ * print_buffer - prints buffer of a char string
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: nothing
  */
 
 void print_buffer(char *b, int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i += 10)
-	{
-		_putchar('0' + i);
-
-		for (j = 0; j < 10; j++)
-		{
-			if ((j + i) >= size)
-			{
-				_putchar('  ');
-			}
-			else
-			{
-				_putchar('0' + *(b + j + i))
-			}
-
-			if ((j % 2) != 0 && j != 0)
-			{
-				_putchar(' ');
-			}
-		}
-
-		for (j = 0; j < 10; j++)
-		{
-			if ((j + i) >= size)
-			{
-				break;
-			}
-			else if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
-			{
-				_putchar('0' + *(b + j + i));
-			}
-			else
-			{
-				_putchar('.');
-			}
-		}
-
-		if (i >= size)
-		{
-			continue;
-		}
-		_putchar('\n');
-	}
 	if (size <= 0)
+		printf("\n");
+	else
 	{
-		_putchar('\n');
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
+			{
+				if (j % 2 == 0 && j != 0)
+					printf(" ");
+				if ((j + i) > size - 1)
+				{
+					printf("  ");
+				}
+				else
+					printf("%.2x", b[i + j]);
+				j++;
+			}
+			printf(" ");
+			j = 0;
+			while (j < 10)
+			{
+				if ((j + i) > size - 1)
+					break;
+				if (b[j + i] <= 31 || b[j + 1] >= '~')
+					b[j + i] = '.';
+				putchar(b[j + i]);
+				j++;
+			}
+			printf("\n");
+		}
 	}
 }
