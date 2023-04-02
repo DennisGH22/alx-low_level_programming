0e99b76d9a360676861adeb90c1cffff531980f4
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 20:05:43 +0200

    Encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -7,41 +8,44 @@
  * Return: s
 */
 
-int i;
+char *leet(char *str)
+{
+	int i;
 
 for (i = 0; i < strlen(str); i++)
-{
-	switch (str[i])
 	{
-		case 'A':
-		case 'a':
-			str[i] = '4';
-			break;
-		case 'E':
-		case 'e':
-			str[i] = '3';
-			break;
-		case 'G':
-		case 'g':
-			str[i] = '6';
-			break;
-		case 'I':
-		case 'i':
-			str[i] = '1';
-			break;
-		case 'O':
-		case 'o':
-			str[i] = '0';
-			break;
-		case 'S':
-		case 's':
-			str[i] = '5';
-			break;
-		case 'T':
-		case 't':
-			str[i] = '7';
-			break;
-		default:
-			break;
+		switch (str[i])
+		{
+			case 'A':
+			case 'a':
+				str[i] = '4';
+				break;
+			case 'E':
+			case 'e':
+				str[i] = '3';
+				break;
+			case 'G':
+			case 'g':
+				str[i] = '6';
+				break;
+			case 'I':
+			case 'i':
+				str[i] = '1';
+				break;
+			case 'O':
+			case 'o':
+				str[i] = '0';
+				break;
+			case 'S':
+			case 's':
+				str[i] = '5';
+				break;
+			case 'T':
+			case 't':
+				str[i] = '7';
+				break;
+			default:
+				break;
+		}
 	}
 }
