3982fe5eda57ef50165758cf1e912fb425c083b8
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 21:58:07 +0200

    Add two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * infinite_add - add two string numbers.
+ * @n: numbers added.
+ * @r: buffer to store the sum.
+ * @size_r: size of buffer.
+ * 
+ * return: r.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j;
+
+	for (i = 0; *(n1 + i); i++)
+	{
+		/**
+		 * Do Nothing...
+		*/
+	}
+
+	for (j = 0; *(n2 + j); j++)
+	{
+		/**
+		 * Do Nothing...
+		*/
+	}
+
+	printf("%d, %d\n", i, j);
+
+	return (r);
+}
