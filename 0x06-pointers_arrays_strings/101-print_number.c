85551d7f308ce7e0ec86b1ca6b5cb65799286958
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 20:48:23 +0200

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,29 +3,20 @@
 /**
  * print_number - Prints an integer.
  * @n: The integer printed.
- *
- * Return: Always 0 (Success).
 */
 
 void print_number(int n)
 {
-	if (n > 0)
-	{
-		if (n >= 10)
-		{
-			_putchar(n / 10);
-		}
-		_putchar('0' + n % 10);
-	}
-	else
+	if (n < 0)
 	{
 		_putchar('-');
-		if (n <= -10)
-		{
-			_putchar(n / -10);
-		}
-		_putchar('0' - n % 10);
+		n -= n;
 	}
 
-	return (0);
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar('0' + n % 10);
 }
