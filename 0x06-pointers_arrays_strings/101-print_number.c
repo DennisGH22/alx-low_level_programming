64bef88a7b0fc359397f0dc5baa9c7f87e4576f7
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 20:36:19 +0200

    Print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer printed.
+ *
+ * Return: Always 0 (Success).
+*/
+
+void print_number(int n)
+{
+	_putchar('0' + n);
+
+	return (0);
+}
