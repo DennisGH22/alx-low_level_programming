3d41cc8a339a8073336f84609b31be72674c217f
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-02 20:00:47 +0200

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,23 @@
 char *cap_string(char *str)
 {
 	int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (i == 0 || str[i - 1] == ' ') {
-            if (str[i] >= 'a' && str[i] <= 'z') {
-                str[i] = str[i] - 'a' + 'A';
-            }
-        }
-        else {
-            if (str[i] >= 'A' && str[i] <= 'Z') {
-                str[i] = str[i] - 'A' + 'a';
-            }
-        }
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 'a' + 'A';
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] - 'A' + 'a';
+			}
+		}
     }
 
 	return (str);
