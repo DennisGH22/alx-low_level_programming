a471aa628703ca125e533d828ea205398af7ab5d
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-07 22:47:05 +0200

    The power recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,15 @@
 int _pow_recursion(int x, int y)
 {
 	int err = (y < 0),
+	base = (y == 1),
 	factOne = (y <= 1);
 
 	if (factOne)
 	{
 		if (err)
-			return(-1);
+			return (-1);
+		else if (base)
+			return (x);
 		return (1);
 	}
 	return (x * _pow_recursion(x, y - 1));
