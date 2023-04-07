191842dbfed1049e6cef4b8c5401529dd3b2f43a
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-07 23:46:47 +0200

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks if num is prime.
+ * @n: The num checked.
+ *
+ * Return: 1 if num is prime, otherwise 0.
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	int i;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
