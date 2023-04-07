3cd77e993346ee6f0c19bde1c53e758f5f324cdd
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-07 22:43:49 +0200

    The power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The base num.
+ * @y: The power num.
+ *
+ * Return: -1 if y < 0 otherwise the calc val.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	int err = (y < 0),
+	factOne = (y <= 1);
+
+	if (factOne)
+	{
+		if (err)
+			return(-1);
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
