7f855a07f5eeca63282d149de5b12b1f2612db84
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-08 00:07:03 +0200

    Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,21 +3,30 @@
 /**
  * is_prime_number - Checks if num is prime.
  * @n: The num checked.
+ * @i: The iterator.
  *
  * Return: 1 if num is prime, otherwise 0.
 */
 
-int i;
+int checker(int n, int i)
+{
+	if (i == n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+	
+	return checker(n, i + 1);
+}
+
+/**
+ * is_prime_number - Checks if num is prime.
+ * @n: The num checked.
+ *
+ * Return: 1 if num is prime, otherwise 0.
+*/
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-
-	for (i = 0; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	return (checker(n, 2));
 }
