eecf03d0b412241ae31d8a41754aabeef3959010
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-08 15:45:55 +0200

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,33 +3,6 @@
 #include <stdlib.h>
 
 /**
- * main - Add positive numbers.
- * @argc: The number of command-line arguments.
- * @argv: Array of pointers to the argument strings.
- *
- * Return: 0 (Success), 1 (Error).
-*/
-
-int main(int argc, char *argv[])
-{
-	int i, num, sum = 0;
-
-	for (i = 1; i < argc; i++)
-	{
-		num = is_positive(argv[i]);
-		if (num == -1)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += num;
-	}
-	printf("%d\n", sum);
-
-	return (0);
-}
-
-/**
  * is_positive - Checks validity of the string.
  * @str: sThe string checked.
  *
@@ -50,4 +23,29 @@ int is_positive(char *str)
 	}
 
 	return (num);
+}
+
+/**
+ * main - Add positive numbers.
+ * @argc: The number of command-line arguments.
+ * @argv: Array of pointers to the argument strings.
+ *
+ * Return: 0 (Success), 1 (Error).
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, num, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		num = is_positive(argv[i]);
+		if (num == -1)
+			printf("Error\n");
+			return (1);
+		sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
