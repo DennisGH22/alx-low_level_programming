e11ebdf751c71f6f8fe6d3ee2f3b971f3c1e73e6
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-08 15:21:06 +0200

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Adds positive numbers..
+ * num_checker - Check if a string is a positive integer.
+ * @str: string to check
+ *
+ * Return: Int val, or -1 if !+int
+ */
+int _is_positive(char *str)
+{
+	int num = 0;
+
+	for (; *str; str++)
+	{
+		if (*str < '0' || *str > '9')
+			return (-1);
+		num = num * 10 + (*str - '0');
+	}
+
+	return (num);
+}
+
+/**
+ * main - Multiplies two numbers.
  * @argc: The number of command-line arguments.
  * @argv: Array of pointers to the argument strings.
  *
- * Return: Always 0 (Success).
+ * Return: Res, or 1.
 */
 
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    
-    for (int i = 1; i < argc; i++) {
-        char *arg = argv[i];
-        int j = 0;
-        
-        while (arg[j]) {
-            if (!isdigit(arg[j])) {
-                printf("Error\n");
-                return (1);
-            }
-            j++;
-        }
-        
-        sum += atoi(arg);
-    }
-    
-    printf("%d\n", sum);
-    return (0);
+	int sum = 0, num;
+
+	for (int i = 1; i < argc; i++) {
+		num = num_checker(argv[i]);
+		if (num < 0) {
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
