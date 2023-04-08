90cedfb68976128de71ac5e566e5eb09d4a71488
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-08 15:41:31 +0200

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 
 /**
+ * is_positive - Checks validity of the string.
+ * @str: sThe string checked.
+ *
+ * Return: Int val, or -1 if !num.
+*/
+
+int is_positive(char *str)
+{
+    int i = 0,
+	num = 0,
+	len = strlen(str);
+
+    for (i = 0; i < len; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		num = num * 10 + (str[i] - '0');
+	}
+
+    return (num);
+}
+
+/**
  * main - Add positive numbers.
  * @argc: The number of command-line arguments.
  * @argv: Array of pointers to the argument strings.
@@ -12,9 +35,8 @@
 
 int main(int argc, char *argv[])
 {
-    int i, num, sum;
+    int i, num, sum = 0;
 
-    sum = 0;
     for (i = 1; i < argc; i++)
     {
         num = is_positive(argv[i]);
@@ -26,29 +48,4 @@ int main(int argc, char *argv[])
     printf("%d\n", sum);
 
     return (0);
-}
-
-/**
- * is_positive - Checks validity of the string.
- * @str: sThe string checked.
- *
- * Return: Int val, or -1 if !num.
-*/
-
-int is_positive(char *str)
-{
-    int i, num, len;
-
-    i = 0;
-    num = 0;
-    len = strlen(str);
-    while (i < len)
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (-1);
-        num = num * 10 + (str[i] - '0');
-        i++;
-    }
-
-    return (num);
 }
