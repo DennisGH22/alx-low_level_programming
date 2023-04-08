487ceda91faa3b2028122a2cd4fc3aaaa8c99255
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-08 14:03:19 +0200

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name.
+ * @argc: The count of command line arguments.
+ * @argv: Array of pointers to the argument strings.
+ *
+ * Return: Always 0 (Success).
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
