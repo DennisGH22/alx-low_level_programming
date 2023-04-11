27fdd8faeb11608c35048271803a66ea4468233f
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-11 16:58:44 +0200

    A pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,8 +17,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (len = 0; str[len] != '\0'; len++)
-		/* Do nothing... */
+	for (len = 0; str[len] != '\0'; len++);
 
 	dup = (char *)malloc((len + 1) * sizeof(char));
 
