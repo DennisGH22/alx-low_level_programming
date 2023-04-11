c11c432f0ecf5952d2b97bc92f77cf214df3ae34
Onalerona Pako <onapako90@gmail.com>
2023-04-11 15:44:08 +0200

    Initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,10 +6,13 @@ char *create_array(unsigned int size, char c)
         return (NULL);
 
     char *array = (char *)malloc(sizeof(char) * size);
+
     if (array == NULL)
         return (NULL);
 
-    for (unsigned int i = 0; i < size; i++)
+    unsigned int i;
+
+    for (i = 0; i < size; i++)
         array[i] = c;
 
     return (array);
