c098f295c1cb7bd53ece5e5e3e66844711d251f9
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-11 16:08:12 +0200

    Initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * create_array - Creates an array of chars, and initializes it with a specific char.
+ * create_array - Creates an array of chars.
  * @size: Array size.
  * @c: The character.
  *
