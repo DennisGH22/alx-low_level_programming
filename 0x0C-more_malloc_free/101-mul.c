1c45ff5eeaf8040b5d796f8fec6ecebe72cdc19c
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 21:38:13 +0200

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,74 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_all_digits - Checks if a string consists of only digits.
- * @str: Pointer to the string to check.
- *
- * Return: 1 if the string contains non-digit characters, 0 otherwise.
-*/
-
-int is_all_digits(char *str)
-{
-	int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] < '0' || str[i] > '9')
-        {
-            return (1);
-        }
-    }
-
-    return (0);
-}
-
-/**
- * _atoi - Converts a string of ASCII digits to an integer.
- * @str: Pointer to the string to convert.
- *
- * Return: The integer value of the string.
-*/
-
-int _atoi(char *str)
-{
-    int i, result = 0;
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        result *= 10;
-        result += str[i] - '0';
-    }
-
-    return (result);
-}
-
-/**
- * main - Multiplies two positive numbers.
- * @argc: The number of arguments.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success, 98 on failure.
-*/
-
+  * main - multiplies two positive numbers
+  * @argc: argument count
+  * @argv: array of arguments
+  * Return: 0 on success, 98 on failure
+  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j, len1, len2;
+	unsigned long int res = 0;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        if (is_all_digits(argv[i]))
-        {
-            printf("Error\n");
-            return (98);
-        }
-    }
+	for (i = 1; i < 3; i++)
+	{
+		len1 = 0;
+		while (argv[i][len1])
+		{
+			if (argv[i][len1] < '0' || argv[i][len1] > '9')
+			{
+				printf("Error\n");
+				return (98);
+			}
+			len1++;
+		}
+		len2 = 1;
+		for (j = 0; j < len1 - 1; j++)
+			len2 *= 10;
+		j = 0;
+		while (argv[i][j])
+		{
+			res += (argv[i][j] - '0') * len2;
+			len2 /= 10;
+			j++;
+		}
+	}
 
-    return (0);
+	printf("%lu\n", res);
+
+	return (0);
 }
