a984781b237ed847095ab995fe1e942a7792e6cd
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 20:43:11 +0200

    Allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
-		ptr[i] = 0;
+		*(ptr + i) = 0;
 
 	return (ptr);
 }
