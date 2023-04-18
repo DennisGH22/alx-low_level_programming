cd21d9d34eb06cd95852e7adda81bba153adc34d
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 21:59:40 +0200

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct Dog - represents a dog with a name, age, and owner
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: This struct represents a dog with a name, age, and owner.
+*/
+
+typedef struct Dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
