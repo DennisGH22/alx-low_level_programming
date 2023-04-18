4fa34343ea41a162ae01e7d09fa18a58d8d3f914
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 21:20:32 +0200

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+int _atoi(const char *s)
+{
+    int n = 0;
+    int sign = 1;
+
+    while (*s)
+    {
+        if (*s == '-')
+            sign = -sign;
+        else if (_isdigit(*s))
+            n = n * 10 + (*s - '0');
+        else
+            return 0;
+        s++;
+    }
+    return sign * n;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
 /**
  * main - Multiplies two positive numbers.
  * @argc: Argument.
@@ -9,26 +37,22 @@
  * Return: Always 0 (Success).
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num1, num2, result;
+    int num1, num2;
 
-	if (argc != 3)
-	{
-		return (98);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	if (num1 == 0 || num2 == 0)
-	{
-		return (98);
-	}
-
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
-	return (0);
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 98;
+    }
+    if (!_isdigit(*argv[1]) || !_isdigit(*argv[2]))
+    {
+        printf("Error\n");
+        return 98;
+    }
+    num1 = _atoi(argv[1]);
+    num2 = _atoi(argv[2]);
+    printf("%d\n", multiply(num1, num2));
+    return 0;
 }
