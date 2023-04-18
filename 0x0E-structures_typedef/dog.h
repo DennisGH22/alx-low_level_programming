100992fb73b4059db8ae7310e6a396e86ff55ca7
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 22:54:26 +0200

    Create a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,3 +16,4 @@ typedef struct dog
 /* Functions Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
