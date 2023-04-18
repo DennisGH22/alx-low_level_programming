84fc314299326056c22b8ca31e416f4c0a094da7
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-18 21:48:24 +0200

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,53 +2,17 @@
 #include <stdio.h>
 
 /**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(const char *argv)
+  * main - Multiplies two positive numbers.
+  * @argc: Argument count.
+  * @argv: Array of arguments.
+  *
+  * Return: 0 on success, 98 on failure.
+*/
+
+int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	while (argv[i])
-	{
-		if (argv[i] < '0' || argv[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(const char *s)
-{
-	int i = 0, result = 0;
-
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
-}
-
-/**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
-int main(int argc, char * const argv[])
-{
-	int i;
+	int i, j, len1, len2;
+	unsigned long int res = 0;
 
 	if (argc != 3)
 	{
@@ -56,14 +20,35 @@ int main(int argc, char * const argv[])
 		return (98);
 	}
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; i < 3; i++)
 	{
-		if (_isdigit(argv[i]))
+		len1 = 0;
+		while (argv[i][len1])
 		{
-			printf("Error\n");
-			return (98);
+			if (argv[i][len1] < '0' || argv[i][len1] > '9')
+			{
+				printf("Error\n");
+				return (98);
+			}
+			len1++;
+		}
+
+		len2 = 1;
+
+		for (j = 0; j < len1 - 1; j++)
+			len2 *= 10;
+
+		j = 0;
+
+		while (argv[i][j])
+		{
+			res += (argv[i][j] - '0') * len2;
+			len2 /= 10;
+			j++;
 		}
 	}
+
+	printf("%lu\n", res);
 
 	return (0);
 }
