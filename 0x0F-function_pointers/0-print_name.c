a79283937f49a6fe90b1a599c793f6d38416808a
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-19 21:51:23 +0200

    Fix errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -9,5 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
