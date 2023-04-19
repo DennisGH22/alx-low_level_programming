f7c4d8a173a62a4fcf0da95ce5f2002af12eedb3
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-19 09:02:39 +0200

    Initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to print.
+ * @f: The function.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	name != NULL ? f(name) : name;
+}
