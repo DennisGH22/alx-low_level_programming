17a4b7646a5072778f8ca919ae3a2587476c9443
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-23 19:13:20 +0200

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	va_start(args, n);
+	unsigned int i;
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		
