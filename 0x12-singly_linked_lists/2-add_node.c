4f45141ef5f09429393db7b38db81c99ab6745fa
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 22:12:47 +0200

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,22 +15,23 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (str == NULL)
+	if (!head)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	new_node->str = malloc(strlen(str) + 1);
 
-	if (new_node->str == NULL)
+	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
+	strcpy(new_node->str, str);
 	new_node->next = *head;
 	*head = new_node;
 
