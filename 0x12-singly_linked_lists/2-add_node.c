c5f11d1977dad7284a2e05c83152b74725cb58b9
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 22:18:19 +0200

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -14,24 +13,30 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
-	if (!head)
-		return (NULL);
+	int i;
+	const char *s = str;
 
 	new_node = malloc(sizeof(list_t));
 
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = malloc(strlen(str) + 1);
+	for (i = 0; s[i]; i++)
+		;
 
-	if (!new_node->str)
+	new_node->len = i;
+	new_node->str = malloc(sizeof(char) * (i + 1));
+
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	strcpy(new_node->str, str);
+	for (i = 0; str[i]; i++)
+		new_node->str[i] = str[i];
+
+	new_node->str[i] = '\0';
 	new_node->next = *head;
 	*head = new_node;
 
