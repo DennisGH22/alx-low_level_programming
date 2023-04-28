508634ac18d6f83eb166a95f8f204aed323dd07e
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 22:55:30 +0200

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,12 @@
 
 void free_list(list_t *head)
 {
-    list_t *current_node;
+	list_t *current_node;
 
-    for (current_node = head; current_node != NULL; current_node = head)
-    {
-        head = head->next;
-        free(current_node->str);
-        free(current_node);
-    }
+	for (current_node = head; current_node != NULL; current_node = head)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
 }
