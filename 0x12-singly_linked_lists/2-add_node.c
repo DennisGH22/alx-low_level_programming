89ec8e19776be7908d43902ce3cf3148d3925404
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 22:31:35 +0200

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,7 @@
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer that points to the first node of the list. 
+ * @head: A pointer that points to the first node of the list.
  * @str: The string to be duplicated.
  *
  * Return: The address of the new element, or NULL if it failed.
@@ -17,20 +17,25 @@ list_t *add_node(list_t **head, const char *str)
 	const char *s = str;
 
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
 	for (i = 0; s[i]; i++)
 		;
+
 	new_node->len = i;
 	new_node->str = malloc(sizeof(char) * (i + 1));
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	for (i = 0; str[i]; i++)
 		new_node->str[i] = str[i];
+
 	new_node->str[i] = '\0';
 	new_node->next = *head;
 	*head = new_node;
