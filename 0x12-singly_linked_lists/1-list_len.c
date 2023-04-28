408e02fdcde6eb0457de44137fa7bf9a21627422
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 21:58:40 +0200

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,12 +10,10 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t count;
+	size_t count;
 
-    for (count = 0; h != NULL; h = h->next)
-    {
-        count++;
-    }
+	for (count = 0; h != NULL; h = h->next)
+		count++;
 
-    return (count);
+	return (count);
 }
