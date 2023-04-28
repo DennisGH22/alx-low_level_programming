74e4554ccd904c5eb044bd68b33f6f14d85398f5
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-04-28 23:47:46 +0200

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
 section .data
-    hello db 'Hello, Holberton',0
-    fmt db '%s',10,0
+  message db 'Hello, Holberton', 10, 0	; message string with new line and null terminator
 
 section .text
-    global main
-    extern printf
+  global main							; make the entry point visible to the linker
 
-main:
-    push hello
-    push fmt
-    call printf
-    add rsp, 16
+  extern printf							; declare printf as an external function
 
-    xor eax, eax
+  main:
+    mov rdi, message					; load the address of the message into RDI
+    xor eax, eax						; zero out EAX register
+    call printf							; call printf with the message as argument
+
+    mov eax, 0							; return 0 from main
     ret
