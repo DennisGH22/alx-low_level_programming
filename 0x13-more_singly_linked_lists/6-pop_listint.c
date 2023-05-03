cfe741c254231f1e878024ed9461711f09e1b387
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-03 22:39:12 +0200

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,6 +5,8 @@
  * pop_listint - Deletes the head node of a listint_t linked list,
  * and returns the head node’s data (n).
  * @head: A pointer to the first node of the list.
+ *
+ * Return: The head node’s data (n), or 0 if empty.
 */
 
 int pop_listint(listint_t **head)
