a22fd335481679dd8791fb6f5b405a32eb71835c
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-03 21:41:42 +0200

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a linked listint_t list.
+ * @h: A pointer to the head node of the list.
+ *
+ * Return: The number of nodes in the list.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	for (count = 0; h != NULL; h = h->next)
+		count++;
+
+	return (count);
+}
