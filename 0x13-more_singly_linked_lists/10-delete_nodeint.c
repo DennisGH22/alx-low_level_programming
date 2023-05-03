6f5c2f0851d95e78ed6d6bfb09244dbf1a69dccf
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-03 23:42:29 +0200

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,33 +11,33 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev;
-    unsigned int i;
+	listint_t *current, *prev;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        current = *head;
-        *head = current->next;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		current = *head;
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 
-    prev = NULL;
-    current = *head;
-    for (i = 0; current != NULL && i < index; i++)
-    {
-        prev = current;
-        current = current->next;
-    }
+	prev = NULL;
+	current = *head;
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		prev = current;
+		current = current->next;
+	}
 
-    if (current == NULL)
-        return (-1);
+	if (current == NULL)
+		return (-1);
 
-    prev->next = current->next;
-    free(current);
+	prev->next = current->next;
+	free(current);
 
-    return (1);
+	return (1);
 }
