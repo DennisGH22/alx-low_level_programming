2c0793d13c9d5fcf8131a6d4f03eb746e950fc63
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-03 23:00:35 +0200

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,5 +17,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; node != NULL && i < index; i++)
 		node = node->next;
 
-	return (i == index) ? node : NULL;
+	return (node);
 }
