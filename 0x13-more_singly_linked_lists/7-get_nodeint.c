a05d9435ff13471df74b397b6a55192e6cb72ecf
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-03 22:47:46 +0200

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,8 @@
  * Return: A pointer to the nth node, or NULL if it does not exist.
 */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head,
+								unsigned int index)
 {
 	listint_t *node = head;
 	unsigned int i;
