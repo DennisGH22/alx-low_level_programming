75de96ba86508fea9ef21eb05bcc1e28f663110e
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-04 23:09:03 +0200

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,28 +4,31 @@
 
 /**
  * free_listint_safe - Frees a listint_t linked list.
- * @h: A pointer to the head node of the list.
+ * @h: A pointer to the head of the linked list.
  *
  * Return: The size of the list that was freed.
 */
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count;
-	listint_t *temp;
+    size_t count = 0;
+    listint_t *current, *temp;
 
-	for (count = 0; *h != NULL && count < 1024; count++)
-	{
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-	}
+    if (h == NULL || *h == NULL)
+        return (0);
 
-	if (count >= 1024)
-	{
-		*h = NULL;
-		exit(98);
-	}
+    for (current = *h; current != NULL; current = temp)
+    {
+        count++;
+        temp = current->next;
+        free(current);
 
-	return (count);
+        if (temp >= current)
+        {
+            *h = NULL;
+            break;
+        }
+    }
+
+    return (count);
 }
