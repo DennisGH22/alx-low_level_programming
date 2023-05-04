e3ee49e778f0b53b4e51613fc0fe5b3c11744552
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-05 00:16:52 +0200

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,35 +11,28 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *runner, *head;
-	size_t current_index, runner_index;
+	size_t count = 0;
+	listint_t *temp = NULL;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current = *h;
-	head = *h;
-	current_index = 0;
-
-	while (head != NULL)
+	if (!(h && *h))
+		return (count);
+	while (*h)
 	{
-		runner = *h;
-		for (runner_index = 0; runner_index < current_index; runner_index++)
+		count++;
+		if (*h > (*h)->next)
 		{
-			if (runner == current)
-			{
-				*h = NULL;
-				return (current_index);
-			}
-			runner = runner->next;
+			temp = *h;
+			*h = (*h)->next;
+			free(temp);
 		}
-		current = head->next;
-		free(head);
-		head = current;
-		current_index++;
+		else
+		{
+			free(*h);
+			*h = NULL;
+		}
 	}
 
 	*h = NULL;
 
-	return (current_index);
+	return (count);
 }
