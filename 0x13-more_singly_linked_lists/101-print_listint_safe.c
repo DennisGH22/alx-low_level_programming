198e1deb2b5bbcc143e7ed124ad27dd7a8a913f5
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-04 22:14:23 +0200

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -23,7 +23,7 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
 
-		if (current <= current->next)
+		if (current < current->next)
 		{
 			printf("-> [%p] %d\n", (void *)current->next, current->n);
 			exit(98);
