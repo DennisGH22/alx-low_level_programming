deb8c102bb21cfaac808db83441816669025defc
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-08 21:57:36 +0200

    Append text at the end of a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * create_file - Appends text at the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL terminated string to add at the end of the file.
+ *
+ * Return: 1 on success, or -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, len, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+
+		bytes_written = write(file, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	if (fchmod(file, mode) == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
