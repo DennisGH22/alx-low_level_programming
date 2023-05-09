3a3a61c91f9d69453e9c668d91813e832f13f957
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-09 23:10:40 +0200

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,89 +2,116 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <elf.h>
+#include "elf.h"
 
-/**
- * print_error - Print the error message.
- * @msg: The error message.
-*/
+void print_error(char *message);
+void print_usage(char *program_name);
 
-void print_error(char *msg) {
-    dprintf(STDERR_FILENO, "Error: %s\n", msg);
-}
+char *get_elf_class(unsigned char elf_class);
+char *get_data_encoding(unsigned char encoding);
+char *get_os_abi(unsigned char os_abi);
 
-/**
- * main - Displays the information contained in the ELF header
- * at the start of an ELF file.
- * @argc: The number of arguments passed to the program.
- * @argv: The array of pointers to the arguments.
-*/
-
-int main(int argc, char **argv) {
-	int file;
+int main(int argc, char **argv)
+{
+    int fd, i;
+    ssize_t nread;
+    Elf64_Ehdr header;
+    char *filename = argv[1];
 
     if (argc != 2)
-        print_error("Usage: elf_header elf_filename"),
-        exit(98);
+    {
+        print_usage(argv[0]);
+        return 98;
+    }
 
-    file = open(argv[1], O_RDONLY);
-    if (file == -1)
-        print_error("Unable to open file"),
-        exit(98);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        print_error("Error opening file");
+        return 98;
+    }
 
-    Elf64_Ehdr header;
-    if (read(file, &header, sizeof(header)) != sizeof(header))
-        print_error("Unable to read ELF header"),
-        exit(98);
+    nread = read(fd, &header, sizeof(header));
+    if (nread != sizeof(header))
+    {
+        print_error("Error reading ELF header");
+        return 98;
+    }
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-        print_error("File is not an ELF file"),
-        exit(98);
-
-    printf("Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++)
+    {
         printf("%02x ", header.e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n", get_elf_class(header.e_ident[EI_CLASS]));
+    printf("  Data:                              %s\n", get_data_encoding(header.e_ident[EI_DATA]));
+    printf("  Version:                           %d%s\n", header.e_ident[EI_VERSION], header.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+    printf("  OS/ABI:                            %s\n", get_os_abi(header.e_ident[EI_OSABI]));
+    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+    printf("  Type:                              0x%x\n", header.e_type);
+    printf("  Entry point address:               0x%llu\n", header.e_entry);
     printf("\n");
 
-    printf("Class:                             ");
-    switch (header.e_ident[EI_CLASS])
-	{
-        case ELFCLASSNONE: printf("none\n"); break;
-        case ELFCLASS32: printf("ELF32\n"); break;
-        case ELFCLASS64: printf("ELF64\n"); break;
-        default: printf("<unknown>\n"); break;
+    if (close(fd) == -1)
+    {
+        print_error("Error closing file");
+        return 100;
     }
 
-    printf("Data:                              ");
-    switch (header.e_ident[EI_DATA]) 
-	{
-        case ELfileATANONE: printf("none\n"); break;
-        case ELfileATA2LSB: printf("2's complement, little endian\n"); break;
-        case ELfileATA2MSB: printf("2's complement, big endian\n"); break;
-        default: printf("<unknown>\n"); break;
+    return 0;
+}
+
+void print_error(char *message)
+{
+    fprintf(stderr, "Error: %s\n", message);
+}
+
+void print_usage(char *program_name)
+{
+    fprintf(stderr, "Usage: %s elf_filename\n", program_name);
+}
+
+char *get_elf_class(unsigned char elf_class)
+{
+    switch (elf_class)
+    {
+        case ELFCLASS32:
+            return "ELF32";
+        case ELFCLASS64:
+            return "ELF64";
+        default:
+            return "Invalid class";
     }
+}
 
-    printf("Version:                           %d\n", header.e_ident[EI_VERSION]);
+char *get_data_encoding(unsigned char encoding)
+{
+    switch (encoding)
+    {
+        case ELFDATA2LSB:
+            return "2's complement, little endian";
+        case ELFDATA2MSB:
+            return "2's complement, big endian";
+        default:
+            return "Invalid encoding";
+    }
+}
 
-    printf("OS/ABI:                            ");
-    switch (header.e_ident[EI_OSABI])
+char *get_os_abi(unsigned char os_abi)
+{
+    switch (os_abi)
 	{
-        case ELFOSABI_SYSV: printf("UNIX - System V\n"); break;
-        case ELFOSABI_HPUX: printf("UNIX - HP-UX\n"); break;
-        case ELFOSABI_NETBSD: printf("UNIX - NetBSD\n"); break;
-        case ELFOSABI_LINUX: printf("UNIX - GNU\n"); break;
-        case ELFOSABI_SOLARIS: printf("UNIX - Solaris\n"); break;
-        case ELFOSABI_AIX: printf("UNIX - AIX\n"); break;
-        case ELFOSABI_IRIX: printf("UNIX - IRIX\n"); break;
-        case ELFOSABI_FREEBSD: printf("UNIX - FreeBSD\n"); break;
-        case ELFOSABI_TRU64: printf("UNIX - TRU64\n"); break;
-        case ELFOSABI_MODESTO: printf("Novell - Modesto\n"); break;
-        case ELFOSABI_OPENBSD: printf("UNIX - OpenBSD\n"); break;
-        case ELFOSABI_ARM: printf("ARM\n"); break;
-        case ELFOSABI_STANDALONE: printf("Standalone App\n"); break;
-        default: printf("<unknown>\n"); break;
+        case ELFOSABI_SYSV: return "UNIX System V ABI";
+        case ELFOSABI_HPUX: return "HP-UX ABI";
+        case ELFOSABI_NETBSD: return "NetBSD ABI";
+        case ELFOSABI_LINUX: return "Linux ABI";
+        case ELFOSABI_IRIX: return "IRIX ABI";
+        case ELFOSABI_FREEBSD: return "FreeBSD ABI";
+        case ELFOSABI_TRU64: return "TRU64 UNIX ABI";
+        case ELFOSABI_ARM: return "ARM architecture ABI";
+        case ELFOSABI_STANDALONE: return "Standalone (embedded) ABI";
+        default: return "Unknown";
     }
 }
