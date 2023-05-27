02d4a53be2632d64ea166bb3371972ebd2c815fd
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-27 20:23:59 +0200

    Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @n: The number.
  * @index: The index.
  *
- * Returns: The value of the bit at index, or -1 if an error occured.
+ * Return: The value of the bit at index, or -1 if an error occured.
 */
 
 int get_bit(unsigned long int n, unsigned int index)
