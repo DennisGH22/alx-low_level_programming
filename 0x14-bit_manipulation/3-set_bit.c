a25b9f20b265c462736aba070350eb60f2956189
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-27 20:35:32 +0200

    Set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,11 +11,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
-
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
+	*n |= 1UL << index;;
 	return (1);
 }
