16e2c56ca4aaa552f4d6aaf302fa135fe7b5a4fa
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-05-27 19:44:22 +0200

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number turned into a binary.
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int flag = 0;
+
+	for (; mask != 0; mask >>= 1;)
+	{
+		if ((n & mask) != 0)
+			flag = 1;
+
+		if (flag)
+			printf("%d", (n & mask) != 0);
+	}
+
+	if (!flag)
+		printf("0");
+}
