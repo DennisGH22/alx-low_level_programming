aed1b4750a21efe4b4b6c17d276f23f90e748b50
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-07-06 21:08:42 +0200

    0x014-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+*/
+
+int get_endianness(void)
+{
+	unsigned int value = 1;
+	char *ptr = (char *)&value;
+
+	return (*ptr);
+}
