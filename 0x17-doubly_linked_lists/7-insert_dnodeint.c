7cdb5b5b0158213abfced48be905193f37530c61
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-01 00:03:54 +0200

    Betty Style guidelines

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,23 +10,16 @@
  * Return: Address of the new node, or NULL if it failed.
 */
 
-dlistint_t *insert_dnodeint_at_index(
-    dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)),
-    *current = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *current = *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
+	if (h == NULL || new_node == NULL)
 		return (NULL);
-
-	if (new_node == NULL)
-		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -49,10 +42,8 @@ dlistint_t *insert_dnodeint_at_index(
 	}
 
 	new_node->next = current->next;
-
 	if (current->next != NULL)
 		current->next->prev = new_node;
-
 	current->next = new_node;
 	new_node->prev = current;
 
