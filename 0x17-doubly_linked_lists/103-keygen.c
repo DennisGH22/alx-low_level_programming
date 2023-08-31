6d3d7ab539e36484e92392d68d73a04965725600
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-01 00:54:01 +0200

    Keygen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,21 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *codex = "vYUKpxOB2QvKjNP1Dm2q24L_wluGfHB_sV9xPKV25sU!N$l&otaCPFWr9bkcj&ou";
-
 /**
- * main - Generates a password based on the username.
- * @argc: The number of command line arguments.
- * @argv: An array containing the command line arguments.
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 upon successful execution.
-*/
-
+ * Return: Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int len = strlen(argv[1]);
-	int i, tmp;
-	char password[7];
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
+
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
 	password[0] = codex[tmp];
@@ -32,7 +30,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
-	int max_char = argv[1][0];
 	for (i = 0; i < len; i++)
 	{
 		if (argv[1][i] > tmp)
@@ -46,11 +43,11 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		tmp += (argv[1][i] * argv[1][i]);
 	password[4] = codex[(tmp ^ 239) & 63];
 
-	for (i = 0; i < max_char; i++)
+	for (i = 0; i < argv[1][0]; i++)
 		tmp = rand();
 	password[5] = codex[(tmp ^ 229) & 63];
 
 	password[6] = '\0';
-	printf("%s\n", password);
-	return 0;
+	printf("%s", password);
+	return (0);
 }
