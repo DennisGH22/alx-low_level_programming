9dbdc21fd4df74d29e8048f476f4ec53735f6d7c
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-01 00:00:32 +0200

    Betty Style guidelines

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,8 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *current = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)),
+    *current = *h;
 	unsigned int i = 0;
 
 	if (h == NULL)
